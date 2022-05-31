90ec46d9b2ca88c46e2f85f3c7960d078f224424
Johannes Ibald <j.ibald@protonmail.ch>
2022-05-31 12:29:33 +0200

    initial seed idead

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+CATEGORIES = ["lead", "chords", "pads", "bassline", "drum pattern", "pluck"]
+MOODS = ["uplifting", "happy", "dramatic", "sad", "melancholic", "cinematic", "aggressive", "chill"]
+
+# generates 20 random midi files
+20.times do
+  # this will be executed 20 times
+  Midi.create(
+    title:
+  )
+end
