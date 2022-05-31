4ca939df8fd99695f40e136bd98564db55319cf8
Johannes Ibald <j.ibald@protonmail.ch>
2022-05-31 16:32:54 +0200

    fixes dependency problem, avoids multiple occurences of same mood/category on one midi

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 require 'faker'
 
+puts "User.destroy_all"
+User.destroy_all
+
 # creates categories based on CATEGORIES array
 CATEGORIES = ["lead", "chords", "pads", "bassline", "drum pattern", "pluck", ""]
 Category.destroy_all
@@ -39,8 +42,6 @@ TIME_SIGNATURES = ["2/4", "3/4", "4/4", "2/2", "3/2", "4/2", "2/8", "3/8", "4/8"
 
 # creates 15 random users
 # User.create(email: "gerald@gmx.de", password: "12345")
-puts "User.destroy_all"
-User.destroy_all
 
 15.times do
   puts "creating a user:"
@@ -55,8 +56,8 @@ User.destroy_all
 end
 
 # generates 20 random midi files
-puts "Midi.destroy_all"
-Midi.destroy_all
+# puts "Midi.destroy_all"
+# Midi.destroy_all
 
 20.times do
   # this will be executed 20 times
@@ -71,24 +72,30 @@ Midi.destroy_all
   )
 
   # creating between 0 and 4 moods for a single midi
+  arr_mood = Mood.all.sample(4)
+  i = 0
   rand(0..4).times do
     MidiMood.create(
       {
         midi: midi,
         # mood: Mood.all.shuffle[0]
-        mood: Mood.all.sample
+        mood: arr_mood[i]
       }
     )
+    i += 1
   end
 
   # creating between 0 and 3 categories for a single midi
+  arr_category = Category.all.sample(4)
+  y = 0
   rand(0..3).times do
     MidiCategory.create(
       {
         midi: midi,
-        category: Category.all.sample
+        category: arr_category[y]
       }
     )
+    y += 1
   end
 
   puts "created #{midi.title}"
