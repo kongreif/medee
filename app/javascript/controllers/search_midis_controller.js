2e5887174d6838021c7c554160cb794ed1e51507
Johannes Ibald <j.ibald@protonmail.ch>
2022-06-01 10:55:12 +0200

    removes unnecessary comments, comments out console.log() in JS controller

--- app/javascript/controllers/search_midis_controller.js
+++ app/javascript/controllers/search_midis_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
   static targets = ["searchform", "searchinput", "midilist"]
 
   connect() {
-    console.log(this.searchformTarget)
-    console.log(this.searchinputTarget)
-    console.log(this.midilistTarget)
+    // console.log(this.searchformTarget)
+    // console.log(this.searchinputTarget)
+    // console.log(this.midilistTarget)
   }
 
   update() {
-    console.log("TODO: send request in AJAX")
+    // console.log("TODO: send request in AJAX")
     const url = `${this.searchformTarget.action}?query=${this.searchinputTarget.value}`
     fetch(url, {headers: {"Accept": "text/plain"}})
     .then(response => response.text())
