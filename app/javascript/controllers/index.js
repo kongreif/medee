9d08819f2a54717816eca9e1b7b3909c3b0416ac
Johannes Ibald <j.ibald@protonmail.ch>
2022-06-01 12:03:05 +0200

    removed import of non-existent hello_controller.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller.js"
-application.register("hello", HelloController)
-
 import SearchMidisController from "./search_midis_controller.js"
 application.register("search-midis", SearchMidisController)
