ab137569d5261da7a235fb710acb3b5c9c19101e
jeremy bordat <j.bordat@live.fr>
2022-06-06 15:55:19 +0200

    options menu -edit and destroy-  added

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :midis, only: %i[index show create new]
+  resources :midis, only: %i[index show create new edit update destroy]
   resources :profiles, only: %i[edit update show]
 
   # get "midis", to: "midis#index", as: :midis
