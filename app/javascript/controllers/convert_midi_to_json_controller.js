527858c4b431c544de5100c59e1ff92e44a3dcf5
Johannes Ibald <j.ibald@protonmail.ch>
2022-06-07 19:50:54 +0200

    basic setup in js controller: convert_midi_to_json_controller

--- app/javascript/controllers/convert_midi_to_json_controller.js
+++ app/javascript/controllers/convert_midi_to_json_controller.js
@@ -7,11 +7,28 @@ export default class extends Controller {
   static targets = ["input", "result"]
 
   connect() {
-    console.log("connect convert midi to json")
+    // console.log("connect convert midi to json")
   }
 
   async toJson() {
     await this.getBase64(this.inputTarget.files[0], this.resultTarget)
+    // console.log(`toJson::${this.inputTarget.files[0]}`)
+    await this.getChartData(this.resultTarget, this.chartDataResultTarget) // TODO check parameters
+  }
+
+  getChartData = async (file, target) => {
+    console.log("getChartData::");
+    console.log(file);
+    // const json = JSON.parse(file);
+    // console.log(json);
+    // var chart_data = [];
+    // const arr_notes = json["tracks"][0]["notes"];
+    // for ( let i = 0; i < arr_notes.length(); i++ )
+    // {
+    //   var tmp_arr = [ arr_notes[i]["time"], arr_notes[i]["midi"] ];
+    //   chart_data.push(tmp_arr);
+    // }
+    // target.value = chart_data
   }
 
   getBase64 = async (file, target) => {
