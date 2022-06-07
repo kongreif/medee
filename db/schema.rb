7c115316625f03d8ed092b08dd81b93e8e0ceb79
Konstantin Greif <konstantin.greif@icloud.com>
2022-06-07 14:53:24 +0200

    Add layout, profile links and most of the styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_131653) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_122810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,12 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_131653) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.bigint "midi_id", null: false
@@ -56,15 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_131653) do
     t.datetime "updated_at", null: false
     t.index ["midi_id"], name: "index_comments_on_midi_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "midi_categories", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.bigint "midi_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_midi_categories_on_category_id"
-    t.index ["midi_id"], name: "index_midi_categories_on_midi_id"
   end
 
   create_table "midi_moods", force: :cascade do |t|
@@ -101,6 +86,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_131653) do
     t.text "bio_description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "soundcloud_link"
+    t.string "instagram_link"
+    t.string "facebook_link"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
@@ -130,8 +118,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_131653) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "midis"
   add_foreign_key "comments", "users"
-  add_foreign_key "midi_categories", "categories"
-  add_foreign_key "midi_categories", "midis"
   add_foreign_key "midi_moods", "midis"
   add_foreign_key "midi_moods", "moods"
   add_foreign_key "midis", "users"
