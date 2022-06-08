460add00913c571d2a834b7a84e804da1de0f67d
Johannes Ibald <j.ibald@protonmail.ch>
2022-06-08 09:42:09 +0200

    commit before adding a chart-data-column to midi model/schema

--- app/javascript/controllers/convert_midi_to_json_controller.js
+++ app/javascript/controllers/convert_midi_to_json_controller.js
@@ -4,21 +4,23 @@ import { Midi } from '@tonejs/midi'
 // Connects to data-controller="convert-midi-to-json"
 export default class extends Controller {
 
-  static targets = ["input", "result"]
+  static targets = ["input", "result", "chart"]
 
   connect() {
     // console.log("connect convert midi to json")
+    console.log(this.chartTarget);
   }
 
   async toJson() {
-    await this.getBase64(this.inputTarget.files[0], this.resultTarget)
+    await this.getBase64(this.inputTarget.files[0], this.resultTarget, this.chartTarget);
     // console.log(`toJson::${this.inputTarget.files[0]}`)
-    await this.getChartData(this.resultTarget, this.chartDataResultTarget) // TODO check parameters
+    // console.log(`toJson::${this.resultTarget}`)
+    // await this.getChartData(this.resultTarget, this.chartDataResultTarget) // TODO check parameters
   }
 
   getChartData = async (file, target) => {
     console.log("getChartData::");
-    console.log(file);
+    console.log(file.value);
     // const json = JSON.parse(file);
     // console.log(json);
     // var chart_data = [];
@@ -31,13 +33,17 @@ export default class extends Controller {
     // target.value = chart_data
   }
 
-  getBase64 = async (file, target) => {
+  getBase64 = async (file, target, target2) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = async function () {
-      const midi = await Midi.fromUrl(reader.result)
-      target.value = JSON.stringify(midi)
+      const midi = await Midi.fromUrl(reader.result);
+      target.value = JSON.stringify(midi);
+      console.log(midi);
+
     };
+
+
  }
 
 }
